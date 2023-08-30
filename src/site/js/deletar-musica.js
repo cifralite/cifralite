05207d9450277db38305688abd4f8a4161251461
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-08-30 09:35:15 -0300

    Ajustada a posição do botão Delete

--- src/site/js/deletar-musica.js
+++ src/site/js/deletar-musica.js
@@ -1,11 +1,12 @@
 const modal = document.querySelector("dialog")
 const btnCancelar = document.getElementById("btn-cancelar")
 const btnConfirmar = document.getElementById("btn-confirmar")
+const cabecalho = document.getElementById("cabecalho")
 
 let btnDeletarMusica = document.createElement('button')
 btnDeletarMusica.classList.add('btn-delete')
 btnDeletarMusica.innerHTML = '<span class="material-symbols-outlined">delete</span>'
-conteudo.appendChild(btnDeletarMusica)
+cabecalho.appendChild(btnDeletarMusica)
 
 // Mostra o Modal
 btnDeletarMusica.onclick = function () {
@@ -18,7 +19,6 @@ btnCancelar.onclick = function () {
 }
 
 // Exclui a música da lista de músicas 
-// Obs.: Não retira a exibição da música exluída
 btnConfirmar.onclick = function () {
     if (musicas.indexOf(musica) >= 0) { 
         musicas.splice(musicas.indexOf(musica), 1)
