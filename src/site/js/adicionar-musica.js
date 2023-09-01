c5e412c3b98e883276fe29dbcefa95646354052e
Yago Cortez R. Fernandes <yagocn45@gmail.com>
2023-09-01 02:48:31 -0300

    adicionar-musica-Alpha

--- src/site/js/adicionar-musica.js
+++ src/site/js/adicionar-musica.js
@@ -1,20 +1,21 @@
+// exibir dialog
 const button = document.getElementById("addDialogButton")
-const modal = document.getElementById("addDialog")
+const modalAdicionando = document.getElementById("addDialog")
 const buttonClose = document.getElementById("buttonClose")
 
 button.onclick = function () {
-    modal.showModal()
+    modalAdicionando.showModal()
 }
 
 buttonClose.onclick = function () {
-    modal.close()
+    modalAdicionando.close()
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const chordsContainer = document.getElementById("chordsContainer");
     const addChordButton = document.querySelector(".add-chord-button");
 
-    addChordButton.addEventListener("click", function() {
+    addChordButton.addEventListener("click", function () {
         const chordInput = document.createElement("div");
         chordInput.classList.add("chord-input");
         chordInput.innerHTML = `
@@ -24,10 +25,42 @@ document.addEventListener("DOMContentLoaded", function() {
         `;
 
         const removeChordButton = chordInput.querySelector(".remove-chord-button");
-        removeChordButton.addEventListener("click", function() {
+        removeChordButton.addEventListener("click", function () {
             chordsContainer.removeChild(chordInput);
         });
 
         chordsContainer.appendChild(chordInput);
     });
 });
+
+// pegar os valores
+document.addEventListener("DOMContentLoaded", function () {
+    const enviar = document.getElementById("buttonEnviar");
+
+    enviar.addEventListener("click", function () {
+        const tempoInput = document.getElementById("buttonTempo");
+        const tomInput = document.getElementById("buttonTom");
+        const tituloInput = document.getElementById("buttonTitulo");
+
+        const tempoValor = tempoInput.value;
+        const tomValor = tomInput.value;
+        const tituloValor = tituloInput.value;
+
+        // console.log("Valor do título:", tituloValor);
+        // console.log("Qual tom está:", tomValor);
+        // console.log("Qual o tempo:", tempoValor);
+
+        const chordsForm = document.getElementById("chordsForm");
+        const chordInputs = chordsForm.querySelectorAll(".chord");
+
+        // Armazena os valores dos acordes
+        const chordValues = [];
+
+        // Itera pelos campos de entrada de acorde e obtém seus valores
+        chordInputs.forEach(function (input) {
+            chordValues.push(input.value);
+        });
+
+        // console.log("Valores dos Acordes:", chordValues);
+    });
+})
