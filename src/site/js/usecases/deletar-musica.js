4d8bd44d2a45c89e4d2f47a6a57b8a2acfa25af6
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-09-25 10:49:01 -0300

    Melhorada a interface do Modal de deletar músicas

--- src/site/js/usecases/deletar-musica.js
+++ src/site/js/usecases/deletar-musica.js
@@ -1,31 +1,24 @@
-const modal = document.getElementById("btn-delete")
-const btnCancelar = document.getElementById("btn-cancelar")
-const btnConfirmar = document.getElementById("btn-confirmar")
-const cabecalho = document.getElementById("cabecalho")
+let modal = document.getElementById("modal-delete")
+let btnDeletarMusica = document.getElementById("btn-delete")
+let btnCancelar = document.getElementById("btn-cancelar")
+let btnConfirmar = document.getElementById("btn-confirmar")
+let cabecalho = document.getElementById("cabecalho")
 
-let btnDeletarMusica = document.createElement('button')
-btnDeletarMusica.classList.add('btn-delete')
-btnDeletarMusica.innerHTML = '<span class="material-symbols-outlined">delete</span>'
-cabecalho.appendChild(btnDeletarMusica)
-
-// Mostra o Modal
 btnDeletarMusica.onclick = function () {
     modal.showModal()
 }
 
-// Fecha o Modal
 btnCancelar.onclick = function () {
     modal.close()
 }
 
-// Exclui a música da lista de músicas 
 btnConfirmar.onclick = function () {
     if (musicas.indexOf(musica) >= 0) {
         musicas.splice(musicas.indexOf(musica), 1)
         SalvarMusicas(musicas)
     }
     modal.close()
-    const divMain = document.getElementById("main")
+    let divMain = document.getElementById("main")
     divMain.removeChild(conteudo)
     divMain.removeChild(cabecalho)
 }
