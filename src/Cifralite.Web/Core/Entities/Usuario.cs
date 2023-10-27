22cc0e822f5ed2a136bdf47225c62b98ea448df7
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-10-27 10:11:58 -0300

    Criado o mapeamento da entidade Usuario

--- src/Cifralite.Web/Core/Entities/Usuario.cs
+++ src/Cifralite.Web/Core/Entities/Usuario.cs
@@ -1,5 +1,6 @@
 namespace Cifralite.Web.Core.Entities{
-    class Usuario {
+    public class Usuario {
+        public int Id { get; set; }
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Senha { get; set; } = string.Empty;
